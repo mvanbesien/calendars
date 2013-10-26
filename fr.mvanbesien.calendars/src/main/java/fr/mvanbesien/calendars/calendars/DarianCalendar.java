5c960835f5cf752b731ef565371fe5f34243dd08
mvanbesien <mvaawl@gmail.com>
2013-10-26 16:25:16 +0200

    Fixed algorithm

--- fr.mvanbesien.calendars/src/main/java/fr/mvanbesien/calendars/calendars/DarianCalendar.java
+++ fr.mvanbesien.calendars/src/main/java/fr/mvanbesien/calendars/calendars/DarianCalendar.java
@@ -3,58 +3,90 @@ package fr.mvanbesien.calendars.calendars;
 import java.util.Calendar;
 
 import fr.mvanbesien.calendars.dates.DarianDate;
-import fr.mvanbesien.calendars.tools.Utils;
 
+/**
+ * Displays a Mars Darian Calendar Date.
+ * 
+ * @author mvanbesien
+ * 
+ */
 public class DarianCalendar {
 
-	private final static double SOL_LENGTH_MS = 88775244.09;
+	/**
+	 * Days difference with Unix Epoch
+	 */
+	private static final float UNIX_EPOCH = 719527;
 
-	private final static double DAY_LENGTH_MS = 86400000.00;
+	/**
+	 * Ration between days length on mars and on earth
+	 */
+	private static final float DAY_RATIO = 1.027491251f;
 
-	private static final double CALENDAR_START = 2308805.7782;
+	/**
+	 * Epoch since beginning of Darian calendar.
+	 */
+	private static final float DARIAN_EPOCH = 587744.77817f;
 
-	private static final long DECADE_LENGTH = 6 * 669 + 4 * 668;
-
-	private static final long[] DECADE_REPARTITION = { 669, 669, 668, 669, 668, 669, 668, 669, 668, 669 };
-
-	private static final long[] YEAR_REPARTITION = { 28, 28, 28, 27, 28, 28, 28, 27, 28, 28, 28, 27, 28, 28, 28, 28 };
-
-	public DarianDate getDate() {
+	/**
+	 * @return Darian Date corresponding to now.
+	 */
+	public static DarianDate getDate() {
 		return getDate(Calendar.getInstance());
 	}
 
+	/**
+	 * @return Darian Date corresponding to calendar passed as parameter..
+	 */
 	public static DarianDate getDate(Calendar calendar) {
 
-		Calendar calendarFromJulianDay = Utils.getCalendarFromJulianDay(CALENDAR_START);
-		double elapsedDays = (calendar.getTimeInMillis() - calendarFromJulianDay.getTimeInMillis()) / DAY_LENGTH_MS;
-		double elapsedSols = (elapsedDays * DAY_LENGTH_MS / SOL_LENGTH_MS);
+		// Computation of Sols
+		double elapsedDays = new Double(calendar.getTimeInMillis()) / 86400000 + UNIX_EPOCH;
+		double elapsedSols = (elapsedDays - DARIAN_EPOCH) / DAY_RATIO;
 
-		int year = 0;
-		while (elapsedSols > DECADE_LENGTH) {
-			year += 10;
-			elapsedSols -= DECADE_LENGTH;
-			if (year % 100 == 0 && year % 500 != 0) {
-				elapsedSols++;
-			}
-		}
+		// First decomposition, based on 500yr basis
+		int halfMilleniums = (int) (elapsedSols / 334296);
+		int halfMilleniumReminder = (int) (elapsedSols - halfMilleniums * 334296);
 
-		for (int i = 0; i < DECADE_REPARTITION.length && elapsedSols > DECADE_REPARTITION[i]; i++) {
-			year++;
-			elapsedSols -= DECADE_REPARTITION[i];
-		}
+		// Second decomposition, based on 100yr basis
+		int centuries = halfMilleniumReminder != 0 ? (halfMilleniumReminder - 1) / 66859 : 0;
+		int centuryReminder = halfMilleniumReminder - (centuries != 0 ? centuries * 66859 + 1 : 0);
 
-		int month = 0;
-		for (int i = 0; i < YEAR_REPARTITION.length && elapsedSols > YEAR_REPARTITION[i]; i++) {
-			month++;
-			elapsedSols -= YEAR_REPARTITION[i];
-		}
+		// Third decomposition, based on 10yr basis
+		int decadeLeapDayValue = centuries == 0 ? 1 : 0;
+		int decades = (centuryReminder + decadeLeapDayValue) / 6686;
+		int decadeReminder = centuryReminder - (decades != 0 ? decades * 6686 - decadeLeapDayValue : 0);
 
+		// Fourth decomposition, based on 2yr basis
+		int twoYrsLeapDayValue = centuries != 0 && decades == 0 ? 0 : 1;
+		int twoYrsPeriod = (decadeReminder - twoYrsLeapDayValue) / 1337;
+		int twoYrsPeriodReminder = decadeReminder - (twoYrsPeriod != 0 ? twoYrsPeriod * 1337 + twoYrsLeapDayValue : 0);
+
+		// Fifth and last decomposition, based on yearly basis
+		int yearLeapDayValue = twoYrsPeriod == 0 && (decades != 0 || (decades == 0 && centuries == 0)) ? 0 : 1;
+		int year = (twoYrsPeriodReminder + yearLeapDayValue) / 669;
+		int yearReminder = twoYrsPeriodReminder - (year != 0 ? 669 - yearLeapDayValue : 0);
+
+		// Now, we put all together to compute the date...
+		int years = 500 * halfMilleniums + 100 * centuries + 10 * decades + 2 * twoYrsPeriod + year;
+
+		int quarter = yearReminder / 167 < 4 ? yearReminder / 167 : 3;
+		int solInQuarter = yearReminder - 167 * quarter;
+		int monthInQuarter = solInQuarter / 28;
+
+		int month = monthInQuarter + 6 * quarter;
+		int sol = yearReminder - monthInQuarter * 28 - quarter + 1;
 		DarianDate date = new DarianDate();
-		date.setOrdinaryDate(0, (int) elapsedSols + 1, month, year);
+		date.setOrdinaryDate(0, sol, month, years);
+		
 		return date;
 	}
-
+	
+	/**
+	 * Main Method.
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		System.out.println(getDate(Calendar.getInstance()));
+		System.out.println(getDate());
 	}
+
 }
